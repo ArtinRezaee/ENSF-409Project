0f6ce5d78787da92148dd52b6998287db3ce0b2b
Satyaki Ghosh <satyakig901@hotmail.com>
2017-04-03 15:46:05 -0700

    satyaki - switched some classes around

---
+++ src/AdminGUI.java
@@ -0,0 +1,36 @@
+// Created by crist on 2017-04-01
+
+public class AdminGUI
+{
+    /*
+    // overriding super functions of parent class Client
+    // TODO: to change them accordingly or add more to override
+    @Override
+    protected void login() {
+        super.login();
+    }
+
+    @Override
+    protected void book() {
+        super.book();
+    }
+
+    @Override
+    protected void search(String source, String dest, String date, int id) {
+        super.search(source, dest, date, id);
+    }
+
+    // methods for class Administrator
+    public void delete () {
+        //TODO
+    }
+
+    public void addFlight () {
+        //TODO
+    }
+
+    public void addFlightGroup() {
+        //TODO
+    }
+    */
+}
